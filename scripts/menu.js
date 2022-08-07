55048a498adb5cbc2e3c88b422ddb0f809042076
bryan-limbo <106204439+bryan-limbo@users.noreply.github.com>
2022-08-06 17:09:47 -0700

    Add files via upload

---
+++ scripts/menu.js
@@ -0,0 +1,10 @@
+(function (d) {
+     // d = document
+
+     const $nav = d.querySelector("nav");
+     const $btn = d.querySelector(".btn-menu");
+
+     $btn.addEventListener("click", function () {
+          $nav.classList.toggle("show");
+     });
+})(document);
